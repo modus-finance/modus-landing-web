f157b5c60c14435839cfb059ecd8a79b00521546
jovicheng <jovicheng@foxmail.com>
2024-10-22 14:14:16 +0800

    fix: style

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,14 @@ function App() {
         </div>
         {/* Nav item */}
         <div className="items-center hidden text-base font-semibold gap-x-6 sm:flex sm:text-sm">
-          <div>Home</div>
+          {/* <div>Home</div> */}
+          {/* <div>Borrow / Lend</div>
           <div>Borrow / Lend</div>
-          <div>Borrow / Lend</div>
-          <div>Borrow / Lend</div>
+          <div>Borrow / Lend</div> */}
         </div>
-        <button className="h-10 rounded-[40px] bg-black px-6 py-0 font-medium text-white">
+        {/* <button className="h-10 rounded-[40px] bg-black px-6 py-0 font-medium text-white">
           Launch APP
-        </button>
+        </button> */}
       </nav>
       {/* main-1 */}
       <main className="flex w-screen flex-col items-center justify-center bg-[#60CDFF] px-4 py-8 xl:py-[73px]">
@@ -32,39 +32,40 @@ function App() {
           <Typewriter
             options={{
               wrapperClassName: "text-[#012994] ml-3",
-              cursorClassName: "Typewriter__cursor text-[#012994] ml-2",
+              cursorClassName:
+                "Typewriter__cursor text-[#012994] ml-4 opacity-50 text-2xl",
               loop: true,
-              cursor: "▍",
+              cursor: "_",
             }}
             onInit={(typewriter) => {
               typewriter
                 .changeDelay(200)
                 .typeString("LRTs")
                 .pauseFor(3000)
-                .deleteAll()
+                .deleteAll(1)
                 .typeString("LSTs")
                 .pauseFor(3000)
-                .deleteAll()
+                .deleteAll(1)
                 .typeString("LP tokens")
                 .pauseFor(3000)
-                .deleteAll()
+                .deleteAll(1)
                 .typeString("Stables")
                 .pauseFor(3000)
-                .deleteAll()
+                .deleteAll(1)
                 .typeString("Synthetics Assets")
                 .pauseFor(3000)
-                .deleteAll()
+                .deleteAll(1)
                 .start();
             }}
           />
         </div>
-        <div className="mt-3 text-base font-medium text-black xl:text-2xl xl:font-bold">
+        <div className="mt-3 text-base font-medium text-black xl:text-2xl">
           On Monad’s first modular lending stack
         </div>
         <button className="mt-6 flex h-10 w-full items-center justify-center rounded-[40px] bg-black px-6 py-0 font-medium text-white sm:mt-[68px] sm:w-auto">
           <img src="./x-logo.svg" className="h-9 w-9" alt="" />
         </button>
-        <div className="mt-3 text-sm font-medium text-black sm:font-bold md:text-base xl:text-2xl">
+        <div className="mt-3 text-sm font-medium text-black md:text-base xl:text-2xl">
           Notifications on to avoid FOMO
         </div>
       </main>
@@ -75,7 +76,7 @@ function App() {
           background: "url(./ltv-bg.svg)",
         }}
       >
-        <div className="flex w-auto flex-col justify-start xl:w-[410px]">
+        <div className="flex w-auto flex-col items-center justify-center xl:w-[410px]">
           <div className="text-2xl font-bold text-center sm:text-left sm:text-5xl sm:font-semibold">
             High LTV lending markets
           </div>
@@ -89,7 +90,7 @@ function App() {
             </button>
           </div>
         </div>
-        <div className="relative mt-8 flex flex-col items-center justify-center rounded-[20px] border-2 border-black bg-white px-[46px] py-8 md:mr-[360px] xl:ml-6 xl:mr-0 xl:px-[56px] xl:py-[44px] 2xl:ml-[190px] 2xl:mt-0">
+        <div className="relative mt-8 flex flex-col items-center justify-center rounded-[20px] border-2 border-black bg-white px-[46px] py-8 xl:ml-6 xl:mr-0 xl:px-[56px] xl:py-[44px] 2xl:ml-[190px] 2xl:mt-0">
           <div className="z-10 text-base font-medium leading-[14px] text-[#000000a6] xl:text-[18px]">
             Monad LST market
           </div>
@@ -122,11 +123,11 @@ function App() {
           className="mt-8 w-[80%] max-w-none xl:mt-0 xl:max-w-[680px] 2xl:w-full"
           alt=""
         />
-        <div className="mb-8 mt-12 flex w-auto flex-col justify-start text-center sm:text-left md:mb-16 xl:-mt-20 xl:mb-0 2xl:w-[437px]">
-          <div className="text-2xl font-bold text-white md:text-[68px] md:leading-[80px] xl:font-semibold">
+        <div className="mb-8 mt-12 flex w-auto flex-col items-center justify-center text-center sm:text-center md:mb-16 xl:-mt-20 xl:mb-0 2xl:w-[437px]">
+          <div className="text-2xl font-bold text-white md:text-5xl xl:font-semibold">
             Auto-strategies
           </div>
-          <div className="mt-2 w-auto text-base font-medium text-white sm:mt-[44px] sm:text-[18px] sm:font-normal sm:leading-[28px] md:w-4/5">
+          <div className="mt-2 w-auto text-base font-medium text-white sm:mt-[32px] sm:text-[18px] sm:font-normal sm:leading-[28px] md:w-4/5">
             Automate your looping and outperform thanks to our optimized, high
             LTV markets.
           </div>
@@ -143,10 +144,10 @@ function App() {
           }}
         >
           <img src="cam-img.svg" className="w-[120px] xl:w-[188px]" alt="" />
-          <div className="mt-8 text-2xl font-bold text-white sm:mt-4 sm:text-[52px] sm:font-semibold sm:leading-[72px]">
+          <div className="mt-8 text-2xl font-bold text-white sm:mt-4 sm:text-[48px] sm:font-semibold sm:leading-[72px]">
             Risk isolated by market
           </div>
-          <div className="mt-1 text-sm font-medium text-white sm:text-base sm:leading-[24px]">
+          <div className="mt-1 text-center text-sm font-medium text-white sm:text-base sm:leading-[24px]">
             Custom lending markets tailored to asset type. One bad apple
             shouldn't spoil the bunch.
           </div>
